eb1c63b40d11e51f02068ce16041c9d4d2d63df4
gabrielaliera <gliera2990@gmail.com>
2023-04-22 13:31:01 -0700

    Create overview card

---
+++ src/components/OverViewCard.jsx
@@ -0,0 +1,25 @@
+
+import "./OverViewCard.css"
+
+//Include the top 3 companies with largest number of employee in past month
+
+//pass a json object of top 3 companies
+const OverViewCard = ({title, top3}) => {
+
+  
+
+  return (
+    <div className='overview-list'>
+        <h3>{title}</h3>
+        {top3 && Object.entries(top3).map(([key, value]) => (
+          <div className="overview-card" key={key}>
+            <h2>{value.name}</h2>
+            <p>No. of Employees: {value.number}</p>
+            <p>Effective Date: {value.date}</p>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default OverViewCard;
