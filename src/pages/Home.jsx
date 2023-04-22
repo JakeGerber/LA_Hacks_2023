4d351c3767bd51dcbe02127cbd58df11c302de6d
gabrielaliera <gliera2990@gmail.com>
2023-04-22 13:31:26 -0700

    Change cards to overview components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,40 @@
 import React, { useEffect, useState} from "react";
-import Card from "../components/Card";
+import OverViewCard from "../components/OverViewCard";
 import {Link} from 'react-router-dom';
 
 
-const Home = () => {
+const Home = ({companies}) => {
     
     var layoff="layoff overview data"
     var breaches="data breaches overview data"
-
+    var top3 = {
+        company1: {
+          name: "Tesla",
+          number: 1000,
+          date: "2022-09-09"
+        },
+        company2: {
+          name: "Apple",
+          number: 800,
+          date: "2022-09-09"
+        },
+        company3: {
+          name: "Microsoft",
+          number: 700,
+          date: "2022-09-09"
+        }
+      };
+      
+    var top3Json = JSON.stringify(top3);
+console.log(top3Json);
+console.log("step");
+console.log(companies)
     return(
         <div className="home">
            <h1>Home page</h1>
            <h2>search bar</h2>
-           <Card title={layoff}/>
-           <Card title={breaches}/>
+           <OverViewCard title={layoff} top3={top3}/>
+           <OverViewCard title={breaches}/>
         </div>
     );
 }
