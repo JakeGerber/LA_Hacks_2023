a7ada56df74c7085ff48b28e68ec3c52531683bc
gabrielaliera <gliera2990@gmail.com>
2023-04-22 07:43:18 -0700

    Add NavBar and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
-      
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/layoff" element/>
+        <Route path="/data-breach" element/>
+        <Route path="/login" element/>
+      </Routes>   
     </>
   )
 }
