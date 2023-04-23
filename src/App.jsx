8d34de3d342996d9219f2a46cb909fa93c71199c
gabrielaliera <gliera2990@gmail.com>
2023-04-22 21:46:06 -0700

    Change json file path

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import './App.css'
 import Home from './pages/Home'
 import Layoff from  './pages/Layoff'
 import CompanyPage from './pages/CompanyPage'
-import warnData from './parser/warn_report.json'
-import top3Layoff from './parser/top3companies.json'
-import top3Breach from './parser/top3companies.json'
-import breachData from "./parser/breach_report.json"
+import BreachMap from './pages/BreachMap'
+import warnData from '../parser/jsondata/warn_report_1.json'
+import top3Layoff from '../parser/jsondata/grouped/top3companies.json'
+import top3Breach from '../parser/jsondata/grouped/top3companies.json'
+import breachData from "../parser/jsondata/breach/breach_report.json"
 
 function App() {
 
@@ -20,7 +21,7 @@ function App() {
         <Route path="/" element={<Home layoff={top3Layoff} breach={top3Breach}/>}/>
         <Route path="/layoff" element={<Layoff companies={warnData}/>}/>
         <Route path="/layoff/:id" element={<CompanyPage data={warnData} />} />
-        <Route path="/data-breach" element/>
+        <Route path="/breach" element={<BreachMap data={breachData}/>}/>
         <Route path="/login" element/>
         
       </Routes>   
