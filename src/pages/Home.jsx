1890b9d0b9f354e523fad4255b79cd6ffdbd1b87
gabrielaliera <gliera2990@gmail.com>
2023-04-22 23:53:14 -0700

    delete dummy code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,9 @@
-import React, { useEffect, useState} from "react";
 import OverViewCard from "../components/OverViewCard";
-import {Link} from 'react-router-dom';
-
 
 const Home = ({layoff, breach}) => {
     
     return(
         <div className="home">
-           <h1>Home page</h1>
-           <h2>search bar</h2>
            <OverViewCard title="Top 3 Layoffs" top3={layoff}/>
            <OverViewCard title="Top 3 Breaches" top3={breach}/>
         </div>
