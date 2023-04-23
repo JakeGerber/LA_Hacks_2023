da975bb50a78649b1daae25dcc5adcb03f23fbea
gabrielaliera <gliera2990@gmail.com>
2023-04-23 00:27:10 -0700

    fix indentation

--- src/pages/Layoff.jsx
+++ src/pages/Layoff.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from "react";
+import {useState} from "react";
 import CardList from "../components/CardList";
 import { Outlet} from "react-router-dom"
 
@@ -6,10 +6,10 @@ const Layoff = ({companies}) => {
     const [orderBy, setOrderBy] = useState("company");    
 
     return(
-            <div className="layoff">
+        <div className="layoff">
             <CardList companies={companies} orderBy={orderBy} setOrderBy={setOrderBy}/>
             <Outlet/>
-            </div>
+        </div>
     );
 }
 
