c7bb5b435b927beb7fe78b044d79ee99b68df36a
gabrielaliera <gliera2990@gmail.com>
2023-04-22 17:24:33 -0700

    Adjust routes and imports jsons

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@ import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import './App.css'
 import Home from './pages/Home'
-import LayOff from  './pages/Layoff'
-import data from './parser/warn_report.json'
+import Layoff from  './pages/Layoff'
+import CompanyPage from './pages/CompanyPage'
+import warnData from './parser/warn_report.json'
+import top3Layoff from './parser/top3companies.json'
+import top3Breach from './parser/top3companies.json'
 
 function App() {
 
-  console.log(data)
+  console.log(warnData)
   return (
     <>
       <NavBar/>
       <Routes>
-        <Route path="/" element={<Home companies={data}/>}/>
-        <Route path="/layoff" element={<LayOff companies={data}/>}/>
+        <Route path="/" element={<Home layoff={top3Layoff} breach={top3Breach}/>}/>
+        <Route path="/layoff" element={<Layoff companies={warnData}/>}/>
+        <Route path="/layoff/:id" element={<CompanyPage data={warnData} />} />
         <Route path="/data-breach" element/>
         <Route path="/login" element/>
+        
       </Routes>   
     </>
   )
