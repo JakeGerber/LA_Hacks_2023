07cb1280884c01fd678de768421a3fa025a481bc
gabrielaliera <gliera2990@gmail.com>
2023-04-23 01:58:26 -0700

    Add card list css

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom';
 import Card from "./Card"
 import { useState } from "react";
+import "./CardList.css"
 
 const CardList = ({ companies }) => {
   const [orderBy, setOrderBy] = useState("effectivedate");
@@ -32,21 +33,20 @@ const CardList = ({ companies }) => {
 
 
   return (
-      <div className='card'>
-        
+      <div>
         <div className="options">
           <div className="order-by">
             <span>Order by:</span>
-            <button style={{ backgroundColor: orderBy === "effectivedate" ? "#0E7C6B" : "#12bca2" }} onClick={() => setOrderBy("effectivedate")}>
+            <button style={{ backgroundColor: orderBy === "effectivedate" ? "#961414" : "#fe7300" }} onClick={() => setOrderBy("effectivedate")}>
               Future/Recent Effective Date
             </button>
-            <button style={{ backgroundColor: orderBy === "oldestdate" ? "#0E7C6B" : "#12bca2" }} onClick={() => setOrderBy("oldestdate")}>
+            <button style={{ backgroundColor: orderBy === "oldestdate" ? "#961414" : "#ff5922"  }} onClick={() => setOrderBy("oldestdate")}>
               Oldest Effective Date
             </button>
-            <button style={{ backgroundColor: orderBy === "noofemployees" ? "#0E7C6B" : "#12bca2" }} onClick={() => setOrderBy("noofemployees")}>
+            <button style={{ backgroundColor: orderBy === "noofemployees" ? "#961414" : "#ff5922"  }} onClick={() => setOrderBy("noofemployees")}>
               Highest No. of Employees
             </button>
-            <button style={{ backgroundColor: orderBy === "company" ? "#0E7C6B" : "#12bca2" }} onClick={() => setOrderBy("company")}>
+            <button style={{ backgroundColor: orderBy === "company" ? "#961414" : "#ff5922"  }} onClick={() => setOrderBy("company")}>
               Company A-Z
             </button>
           </div>
