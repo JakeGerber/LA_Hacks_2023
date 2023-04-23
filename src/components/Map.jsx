7427979daa925fc161f4c5925f9bf8d6680867eb
gabrielaliera <gliera2990@gmail.com>
2023-04-23 03:03:16 -0700

    Create custom marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,52 +1,61 @@
 import { useState, useEffect } from "react";
 import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const Map = ({ address }) => {
-    const [latitude, setLatitude] = useState(null);
-    const [longitude, setLongitude]  = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude]  = useState(null);
 
-    const address2 = "1600 Pennsylvania Avenue NW, Washington, DC 20500";
-    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURI(address)}`;
-    const zoomLevel = 13;
+  const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURI(address)}`;
+  const zoomLevel = 13;
 
-    const createMap = (lat, lng) => {
-        // Create a Leaflet map and set its initial view to the location you want to display
-        var map = L.map('mapid',
-                        {scrollWheelZoom:false,
-                         zoomDelta: 0.25,
-                         zoomSnap: 0})
-                  .setView([lat, lng], zoomLevel);
+  const createMap = (lat, lng) => {
+    // Create a custom marker icon
+    const customIcon = L.icon({
+      iconUrl: markerIcon,
+      iconRetinaUrl: markerIcon2x,
+      shadowUrl: markerShadow,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
 
-        // Add a tile layer to the map to display the OpenStreetMap tiles
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-            maxZoom: 18,
-        }).addTo(map);
+    // Create a Leaflet map and set its initial view to the location you want to display
+    var map = L.map('mapid', {scrollWheelZoom:false, zoomDelta: 0.25, zoomSnap: 0})
+              .setView([lat, lng], zoomLevel);
 
-        // Add a marker for the location you want to display
-        var marker = L.marker([lat, lng]).addTo(map);  
-    };
+    // Add a tile layer to the map to display the OpenStreetMap tiles
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
 
-    useEffect(() => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                setLatitude(data[0].lat);
-                setLongitude(data[0].lon);
-            })
-            .catch(error => console.error(error));
-    }, [url]);
+    // Add a marker for the location you want to display
+    L.marker([lat, lng], {icon: customIcon}).addTo(map);  
+  };
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setLatitude(data[0].lat);
+        setLongitude(data[0].lon);
+      })
+      .catch(error => console.error(error));
+  }, [url]);
     
-    useEffect(() => {
-        if (latitude && longitude) {
-            createMap(latitude, longitude);
-        }
-    }, [latitude, longitude]);
+  useEffect(() => {
+    if (latitude && longitude) {
+      createMap(latitude, longitude);
+    }
+  }, [latitude, longitude]);
 
-    return (
-        <div id="mapid"></div>
-    );
+  return (
+    <div id="mapid"></div>
+  );
 };
 
 export default Map;
-
